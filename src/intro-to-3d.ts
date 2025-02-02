2835a334b609ba88b044813efdc42ac3cdd90d17
Mustafa Ahmed <mustafa.ahmed1118@gmail.com>
2025-02-01 19:36:53 -0500

    added shader source code

--- src/intro-to-3d.ts
+++ src/intro-to-3d.ts
@@ -1,3 +1,33 @@
 import { showError } from "./gl-utils";
 
+//////////////////////
+//SHADER SOURCE CODE//
+/////////////////////
+const vertexShaderSourceCode = `#version 300 es
+precision mediump float;
+
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+out vec3 fragmentColor;
+
+uniform mat4 matWorld;
+uniform mat4 matViewProj;
+
+void main() {
+  fragmentColor = vertexColor;
+
+  gl_Position = matViewProj * matWorld * vec4(vertexPosition, 1.0);
+}`;
+
+const fragmentShaderSourceCode = `#version 300 es
+precision mediump float;
+
+in vec3 fragmentColor;
+out vec4 outputColor;
+
+void main() {
+  outputColor = vec4(fragmentColor, 1.0);
+}`;
+
 showError('Hello, 3D WebGL Tutorial!!!!')
